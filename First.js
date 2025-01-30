c8f64836bba8a176f10d3e8caef501f73d873f0b
Developer Afzal <ibn2990@gmail.com>
2025-01-29 19:19:10 -0800

    Add files via upload

---
+++ First.js
@@ -0,0 +1,25 @@
+console.log('Start Loving Javascript');
+console.log('Written by M Afzal');
+console.log('Programming Language = Javascript');
+let developer = "Developer Afzal";
+console.log(developer);
+let object1 = "Product";
+console.log(object1);
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 560,
+}
+console.log(product);
+let object2 = "Profile";
+console.log(object2);
+
+const profile = {
+    username: "developer-afzal",
+    posts: 4,
+    followers: 55,
+    following: 12,
+    description: "Hi, I am a web developer. Passionated about daily tasks of my development. Want to standalone in the market. Love you. Bye ❤❤ "
+}
+console.log(profile);
